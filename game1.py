c113303a941741208df3ed30f2eb693e90028827
SergeyMoroz0703 <sergeymoroz0307@gmail.com>
2017-04-21 17:51:39 +0300

    v2104

--- game1.py
+++ game1.py
@@ -1,10 +1,13 @@
 import pygame
 import random
+import os
+from os import path
 
-WIDTH = 800
-HEIGHT = 600
 
-FPS = 30
+WIDTH = 1250
+HEIGHT = 650
+
+FPS = 60
 #define colors
 WHITE = (255,255,255)
 BLACK = (0,0,0)
@@ -12,21 +15,10 @@ RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
 
-
-
-class Player(pygame.sprite.Sprite):
-    #sprite for the player
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(GREEN)
-        self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
-
-    def update(self):
-        self.rect.x += 5
-        if self.rect.left > WIDTH:
-            self.rect.right = 0
+# set up asserts  folders
+#Windows: "C:\Moroz Python\BooktTask"
+game_folder = os.path.dirname(__file__)
+img_dir = os.path.join(game_folder, "image")
 
 
 
@@ -38,10 +30,133 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("First Game")
 clock = pygame.time.Clock()
 
+
+
+
+class Player(pygame.sprite.Sprite):
+    #sprite for the player
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(player_img, (70,80))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.radius = 30
+        #pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+        self.speedy = 0
+
+    def update (self):
+        self.speedx = 0
+        self.speedy = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -7
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = +7
+        if keystate[pygame.K_DOWN]:
+            self.speedy = + 7
+        if keystate[pygame.K_UP]:
+            self.speedy = - 7
+        self.rect.y += self.speedy
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.top > HEIGHT:
+            self.rect.top = HEIGHT
+        if self.rect.bottom < 0:
+            self.rect.bottom = 0
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image_orig = random.choice(monster_img)
+        self.image_orig.set_colorkey(BLACK)
+        self.image = self.image_orig.copy()
+        self.rect = self.image.get_rect()
+        self.radius = int(self.rect.width * .7 / 2)
+        #pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(- 100, -40)
+        self.speedy = random.randrange(1, 8)
+        self.speedx = random.randrange(-3, 3)
+        self.rot = 0
+        self.rot_speed = random.randrange(-8, 8)
+        self.last_update = pygame.time.get_ticks()
+
+
+    def rotate(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > 50:
+            self.last_update = now
+            self.rot = (self.rot + self.rot_speed) % 360
+            new_image = pygame.transform.rotate(self.image_orig, self.rot)
+            old_center = self.rect.center
+            self.image = new_image
+            self.rect.center = old_center
+
+
+    def update(self):
+        self.rotate()
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(- 100, -40)
+            self.speedy = random.randrange(3, 9)
+
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(bullet_img, (60, 50))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = - 10
+        if self.rect.bottom < 0:
+            self.kill()
+
+
+    def update(self):
+        self.rect.y += self.speedy
+
+
+
+# load all game graphips
+
+
+
+background = pygame.image.load(path.join(img_dir, "background3.jpg")).convert()
+background_rect = background.get_rect()
+player_img = pygame.image.load(path.join(img_dir, "myship2.png")).convert()
+bullet_img = pygame.image.load(path.join(img_dir, "bullet1.png")).convert()
+monster_img = []
+monster_list = ['monster1.png',  'monster3.png', 'monster4.png']
+
+for img in monster_list:
+    monster_img.append(pygame.image.load(path.join(img_dir, img)).convert())
+
 all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
-
+for i in range(8):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
 
 
 
@@ -55,17 +170,35 @@ while running:
         #check for closing window
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
 
 
     #Update
     all_sprites.update()
 
 
+    #check to see if a bullet hit a mob
+    hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+    for hit in hits:
+        m = Mob()
+        all_sprites.add(m)
+        mobs.add(m)
+
+
+    #check to see if mob hit the player
+    hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
+    if hits:
+        running = False
+
 
     #Draw / render
     screen.fill(BLACK)
+    screen.blit(background, background_rect)
     all_sprites.draw(screen)
     # after drawing everything, flip the display
     pygame.display.flip()
 
 pygame.quit()
+
