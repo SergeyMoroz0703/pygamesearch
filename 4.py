884fa7c90a5b677f778c42dd85de6b7071d03010
Serhii Moroz <smoroz@atlassian.com>
2019-12-28 16:45:03 +0200

    asdasd3

---
+++ 4.py
@@ -0,0 +1,23 @@
+def fetch(path,fromTime,untilTime=None,now=None):
+  fh = None
+  try:
+    fh = open(path,'rb')
+    return file_fetch(fh, fromTime, untilTime, now)
+  finally:
+    if fh:
+      fh.close()
+
+
+
+
+
+
+
+
+
+print('asdd')
+
+
+
+def blabla(bla):
+    print('helollllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll')
